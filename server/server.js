97a455a9b028f17d574d55e2ea981b0d0d6a9fab
NargesSh <narges_shaker@ymail.com>
2025-11-18 11:02:22 +0000

    handle CORE error for production url

--- server/server.js
+++ server/server.js
@@ -15,7 +15,7 @@ const dataFile = path.join(__dirname, "data.json");
 
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: ["http://localhost:5173", "http://localhost:4173"],
     methods: ["GET", "POST", "DELETE", "OPTIONS"],
     allowedHeaders: ["Content-Type", "Authorization"],
     credentials: true,
